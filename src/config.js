240b24afb72cf7ee694f518727f5f7b55bfa9912
AbdulWajid768 <abdul45.wajid@gmail.com>
2025-03-11 00:21:06 +0500

    init: initial commit

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// export const API_BASE_URL = 'https://gist.serveo.net';
-export const API_BASE_URL = 'http://127.0.0.1:8000';
-
-export const GOOGLE_CLIENT_ID = '198586848581-i0d7lcs08iki6gtfn4inrj4qgk84e8c1.apps.googleusercontent.com';
-// export const GOOGLE_CLIENT_ID = '784372155904-glhnqam6nt9gseiajm7ke0q2k8813ggs.apps.googleusercontent.com';
-export const ZOOM_AUTHORIZATION_URL = 'https://zoom.us/oauth/authorize?response_type=code&client_id=XloHsIcHSz2HU8dRgHTvA&redirect_uri=https://stg.textalize.ai/connect-zoom/';
-
-export const ENVIRONMENT = 'local'
